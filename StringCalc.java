537389c89e39762b360b803cdfd51e9f71a69f3c
unknown <mungekar.kiran30@gmail.com>
2020-06-10 22:39:44 +0530

    1.Create a simple String calculator with a method signature

---
+++ StringCalc.java
@@ -0,0 +1,31 @@
+import java.util.Arrays;
+
+import java.util.List;
+import java.util.Optional;
+
+
+public class StringCalc{
+
+    public String input= "4,2,1";
+
+    public static void main(String[] args){
+        
+        int total= getInputString("");
+        System.out.println(total);
+
+    }
+    
+   
+    //simple String calculator with a method signature:
+    public static int getInputString(String numbers){
+      if(numbers.length() > 0){
+        List<String> extractIntegers= Arrays.asList(numbers.split(","));
+        int val= extractIntegers.stream().map(num->Integer.parseInt(num)).reduce((total, order)-> total+order).get();
+        return val;
+      }
+      return 0;
+    }
+    
+
+
+}

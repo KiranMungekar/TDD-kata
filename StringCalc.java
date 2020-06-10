68ff3def1c5eb5145f6ed05190bc06aa1231e1e0
unknown <mungekar.kiran30@gmail.com>
2020-06-11 00:15:22 +0530

    4.Support different delimiters

--- StringCalc.java
+++ StringCalc.java
@@ -1,6 +1,6 @@
+import java.util.*; 
 import java.util.Arrays;
 import java.util.List;
-
 
 public class StringCalc{
 
@@ -8,7 +8,7 @@ public class StringCalc{
 
     public static void main(String[] args){
         
-        int total= getInputString("1\n2,3\n4");
+        int total= getInputString("//;\n1;2;3;4");
         System.out.println(total);
 
     }
@@ -17,14 +17,32 @@ public class StringCalc{
     //simple String calculator with a method signature:
     public static int getInputString(String numbers){
       if(numbers.length() > 0){
-        List<String> extractIntegers= Arrays.asList(numbers.split("[,\n]"));
+        Map<String,String> inputs=getDelimiter(numbers);
+        System.out.println(inputs);
+        List<String> extractIntegers= Arrays.asList(inputs.get("inputString").split(inputs.get("delimiter")));
         System.out.println(extractIntegers);
-        int val= extractIntegers.stream().map(num->Integer.parseInt(num)).reduce((total, order)-> total+order).get();
+        int val= extractIntegers.stream().map(num->Integer.parseInt(num.trim())).reduce((total, order)-> total+order).get();
         return val;
       }
       return 0;
     }
     
+
+    public static Map<String ,String> getDelimiter(String inputString){
+     // JSONObject outputObj=new JSONObject();
+      Map<String ,String> outputs=new HashMap<>();
+      String defaultDelimiters=",\n";
+      String defaultString=inputString;
+      if(inputString.startsWith("//")){
+        String delimiter= inputString.substring(0, 4);
+        defaultDelimiters= defaultDelimiters.concat(delimiter);
+        String transformedString= inputString.substring(4, inputString.length());
+        defaultString= transformedString;
+      }
+      outputs.put("delimiter","["+defaultDelimiters+"]");
+      outputs.put("inputString", defaultString);
+      return outputs;
+    }
 
 
 }

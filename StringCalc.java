fd3e8d9c1dcede75858c08e4c6d6d2ff1ed9fbdc
unknown <mungekar.kiran30@gmail.com>
2020-06-10 23:00:01 +0530

    2.Allow the Add method to handle an unknown amount of numbers && 3. Allow the Add method to handle new lines between numbers (instead of commas)

--- StringCalc.java
+++ StringCalc.java
@@ -8,7 +8,7 @@ public class StringCalc{
 
     public static void main(String[] args){
         
-        int total= getInputString("");
+        int total= getInputString("1\n2,3\n4");
         System.out.println(total);
 
     }
@@ -17,7 +17,8 @@ public class StringCalc{
     //simple String calculator with a method signature:
     public static int getInputString(String numbers){
       if(numbers.length() > 0){
-        List<String> extractIntegers= Arrays.asList(numbers.split(","));
+        List<String> extractIntegers= Arrays.asList(numbers.split("[,\n]"));
+        System.out.println(extractIntegers);
         int val= extractIntegers.stream().map(num->Integer.parseInt(num)).reduce((total, order)-> total+order).get();
         return val;
       }
